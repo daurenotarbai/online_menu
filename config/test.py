beaf51fa98d88a00bf390c13a41db1f359bc27f0
daurenotarbai.00 <daurenotarbai.00@gmail.com>
2022-02-26 01:06:45 +0300

    added almahanym

--- config/test.py
+++
@@ -1,5 +0,0 @@
-import qrcode
-
-qr = qrcode.make("http://138.68.74.218/menu/spicyx/")
-print("SSSS")
-qr.save('spicyx_qr.jpg')
--- menu_app/views.py
+++ menu_app/views.py
@@ -1,105 +1,105 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
-# from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
-from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.shortcuts import redirect, render
 from menu_app.forms import UserLoginForm, ProductForm, CategoryForm
 from menu_app.models import Product, Category, Restaurant
 
 
 class MainView(TemplateView):
-  template_name = "main.html"
+    template_name = "main.html"
+
 
 class MenuView(DetailView):
-  model = Restaurant
-  template_name = "menu/menu2.html"
-  context_object_name = "restaurant"
+    model = Restaurant
+    template_name = "menu/menu2.html"
+    context_object_name = "restaurant"
 
-  def get_context_data(self, **kwargs):
-    if kwargs.get('object').slug=='bybrothers':
-      self.template_name='menu/menu4.html'
+    def get_context_data(self, **kwargs):
+        if kwargs.get('object').slug in ['bybrothers', 'almahanym']:
+            self.template_name = 'menu/menu4.html'
 
-    context = super(MenuView, self).get_context_data(**kwargs)
-    context['form_login'] = UserLoginForm(self.request.POST or None)
-    context['form_product'] = ProductForm(self.request.POST or None)
-    context['form_category'] = CategoryForm(self.request.POST or None)
-    return context
+        context = super(MenuView, self).get_context_data(**kwargs)
+        context['form_login'] = UserLoginForm(self.request.POST or None)
+        context['form_product'] = ProductForm(self.request.POST or None)
+        context['form_category'] = CategoryForm(self.request.POST or None)
+        return context
+
 
 @csrf_exempt
 def login_view(request):
-  next = request.GET.get('next')
-  form = UserLoginForm(request.POST or None)
-  if form.is_valid():
+    next = request.GET.get('next')
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
 
-    username = form.cleaned_data.get('username')
-    password = form.cleaned_data.get('password')
-    user = authenticate(username=username, password=password)
-    slug = user.restaurant.first().slug
-    login(request, user)
-    if next:
-      return redirect(next)
-    return redirect('/menu/{}'.format(slug))
-  return render(request, "404.html")
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        slug = user.restaurant.first().slug
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('/menu/{}'.format(slug))
+    return render(request, "404.html")
 
 
 def logout_view(request):
-  logout(request)
-  return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+    logout(request)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
 
 
 class ProductCreateView(CreateView):
-  model = Product
-  fields = ['name','price','category','img','description']
+    model = Product
+    fields = ['name', 'price', 'category', 'img', 'description']
 
-  def get_success_url(self):
-    return self.request.META.get('HTTP_REFERER')
+    def get_success_url(self):
+        return self.request.META.get('HTTP_REFERER')
 
 
 class ProductUpdateView(UpdateView):
-  model = Product
-  fields = ['name','price','img','description']
+    model = Product
+    fields = ['name', 'price', 'img', 'description']
 
-  def get_success_url(self):
-    return self.request.META.get('HTTP_REFERER')
+    def get_success_url(self):
+        return self.request.META.get('HTTP_REFERER')
 
 
 class ProductDeleteView(DeleteView):
-  model = Product
+    model = Product
 
-  def get_success_url(self):
-    return self.request.META.get('HTTP_REFERER')
+    def get_success_url(self):
+        return self.request.META.get('HTTP_REFERER')
 
 
-def update_product_status(request,pk):
-  product = Product.objects.get(pk=pk)
-  if product.is_active:
-      product.is_active = False
-  else:
-      product.is_active = True
-  product.save()
-  return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+def update_product_status(request, pk):
+    product = Product.objects.get(pk=pk)
+    if product.is_active:
+        product.is_active = False
+    else:
+        product.is_active = True
+    product.save()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
 
 
 class CategoryCreateView(CreateView):
-  model = Category
-  fields = ['name','place_order','img','restaurant']
+    model = Category
+    fields = ['name', 'place_order', 'img', 'restaurant']
 
-  def get_success_url(self):
-    return self.request.META.get('HTTP_REFERER')
+    def get_success_url(self):
+        return self.request.META.get('HTTP_REFERER')
 
 
 class CategoryUpdateView(UpdateView):
-  model = Category
-  fields = ['name','img','place_order']
+    model = Category
+    fields = ['name', 'img', 'place_order']
 
-  def get_success_url(self):
-    return self.request.META.get('HTTP_REFERER')
+    def get_success_url(self):
+        return self.request.META.get('HTTP_REFERER')
 
 
 class CategoryDeleteView(DeleteView):
-  model = Category
+    model = Category
 
-  def get_success_url(self):
-    return self.request.META.get('HTTP_REFERER')
+    def get_success_url(self):
+        return self.request.META.get('HTTP_REFERER')
